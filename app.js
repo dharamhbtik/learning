04ecc715d7993de9fde8fefcf05ed19c02982c9a
Dharmendra Kumar <dharmendra.hbti@gmail.com>
2022-04-17 20:30:26 +0530

    refactor

--- app.js
+++
@@ -1,7 +0,0 @@
-const names = require("./nodeJSBasics/names");
-const sayHi = require("./nodeJSBasics/utils");
-require("./nodeJSBasics/mind-grenade");
-console.log(names);
-sayHi(names.john);
-sayHi(names.peter);
-sayHi("Dharmendra");
