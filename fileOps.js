c3c1c8c60aba976071e44c97dd9049d41ed9ec80
Dharmendra Kumar <dharmendra.hbti@gmail.com>
2022-04-17 18:26:56 +0530

    updating the branch

--- fileOps.js
+++ fileOps.js
@@ -11,7 +11,19 @@ const getText = (path) => {
     });
   });
 };
-
-getText("./content/first.txt")
+const path1 = "./content/first.txt";
+const path2 = "./content/second.txt";
+getText(path1)
   .then((result) => console.log(result))
   .catch((err) => console.log(err));
+
+const start = async () => {
+  try {
+    const first = await getText(path1);
+    const second = await getText(path2);
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
